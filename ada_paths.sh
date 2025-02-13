63a3b38ba0da559d1231c2da57bd860bf783dde9
T. Kurt Bond <tkurtbond@gmail.com>
2025-02-13 12:18:36 -0500

    Add a shell script to sourcce to set ADA_INCLUDE_PATH and ADA_OBJECTS_PATH.

---
+++ ada_paths.sh
@@ -0,0 +1,5 @@
+# source this, don't execute it!
+
+export ADA_INCLUDE_PATH=/usr/local/sw/versions/AdaCurses/20211021/share/ada/adainclude
+export ADA_OBJECTS_PATH=/usr/local/sw/versions/AdaCurses/20211021/lib/ada/adalib
+      
